6bd136011c4c20444e96823fa74e965e728f2930
Ruslan <rinsterr@yandex.ru>
2025-08-01 21:09:07 +0300

    oops, forgot something

---
+++ build/CMakeFiles/mem_trainer.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/EpicR/Desktop/mem_trainer/main.cpp" "CMakeFiles/mem_trainer.dir/main.cpp.obj" "gcc" "CMakeFiles/mem_trainer.dir/main.cpp.obj.d"
+  "C:/Users/EpicR/Desktop/mem_trainer/src/MainLoop.cpp" "CMakeFiles/mem_trainer.dir/src/MainLoop.cpp.obj" "gcc" "CMakeFiles/mem_trainer.dir/src/MainLoop.cpp.obj.d"
+  "C:/Users/EpicR/Desktop/mem_trainer/src/RandomGenerators.cpp" "CMakeFiles/mem_trainer.dir/src/RandomGenerators.cpp.obj" "gcc" "CMakeFiles/mem_trainer.dir/src/RandomGenerators.cpp.obj.d"
+  "C:/Users/EpicR/Desktop/mem_trainer/src/TaskGenerator.cpp" "CMakeFiles/mem_trainer.dir/src/TaskGenerator.cpp.obj" "gcc" "CMakeFiles/mem_trainer.dir/src/TaskGenerator.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mem_trainer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for mem_trainer.
+# This may be replaced when dependencies are built.
